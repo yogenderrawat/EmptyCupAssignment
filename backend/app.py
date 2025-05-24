6d66c65102ce6c2bf84a080c9c46f5e51bb156e5
yogender rawat <rawaty634@gmail.com>
2025-05-24 15:18:38 +0530

    workdone again

--- backend/app.py
+++ backend/app.py
@@ -14,4 +14,7 @@ def get_designers():
     return jsonify(data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
